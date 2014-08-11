294cbd839be244ab4d0f755b9bdf4f575ba0d0ef
dmitriy.mukhin <dmitriy.mukhin@gmail.com>
2014-08-10 23:23:38 -0400

    added TextArea console

--- img-applet/src/img_applet/ImgApplet.java
+++ img-applet/src/img_applet/ImgApplet.java
@@ -1,17 +1,23 @@
 package img_applet;
 
 import java.awt.Color;
+import java.awt.TextArea;
 
 import javax.swing.JApplet;
 
 
 @SuppressWarnings("serial")
 public class ImgApplet extends JApplet implements Runnable {
+	
+	public TextArea console;
 
 	@Override
 	public void run() {
+		getContentPane().add(console = new TextArea());
+		console.setEditable(false);
 		getContentPane().setBackground(Color.WHITE);
 		System.out.println(ffmpeg.ffmpeg.ffmpeg.getAbsolutePath());
+		console.append(ffmpeg.ffmpeg.ffmpeg.getAbsolutePath() + "\n");
 	}
 
 	@Override
