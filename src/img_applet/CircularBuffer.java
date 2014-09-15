2c5a8a7300671096c1453a106362230c355a575e
dmitriy.mukhin <dmitriy.mukhin@gmail.com>
2014-09-14 22:49:43 -0400

    fMP4 demuxer contd. (2)

--- src/img_applet/CircularBuffer.java
+++ src/img_applet/CircularBuffer.java
@@ -11,6 +11,24 @@ public class CircularBuffer {
 	public int getInt(int i) { return ptr > 0 ?
 			get(i) << 24 | get(i + 1) << 24 >>> 8 | get(i + 2) << 24 >>> 16 | get(i + 3) << 24 >>> 24 :
 			buf[i] << 24 | buf[i + 1] << 24 >>> 8 | buf[i + 2] << 24 >>> 16 | buf[i + 3] << 24 >>> 24; }
+	public boolean check(int i, char ... cs) {
+		if (ptr > 0) {
+			for (int j = 0; j < cs.length; j++)
+				if (get(i + j) != (byte)cs[j])
+					return false;
+		} else {
+			for (int j = 0; j < cs.length; j++)
+				if (buf[i + j] != (byte)cs[j])
+					return false;
+		}
+		return true;
+	}
+	public boolean check4(int i, char c0, char c1, char c2, char c3) {
+		if (ptr > 0)
+			return get(i) == (byte)c0 && get(i + 1) == (byte)c1 && get(i + 2) == (byte)c2 && get(i + 3) == (byte)c3;
+		else
+			return buf[i] == (byte)c0 && buf[i + 1] == (byte)c1 && buf[i + 2] == (byte)c2 && buf[i + 3] == (byte)c3;
+	}
 	public int read(byte[] b, int off, int len) {
 		int len_ = len < this.len ? len : this.len;
 		for (int i = 0; i < len_; i++)
--- src/img_applet/fMP4DemuxerInputStream.java
+++ src/img_applet/fMP4DemuxerInputStream.java
@@ -16,25 +16,41 @@ public class fMP4DemuxerInputStream extends FilterInputStream implements Demuxer
 
 	private int pos;
 	private CircularBuffer buf = new CircularBuffer(8);
+	private boolean dirty = true;
 	private long skip_(long n) throws IOException {
-		if (n <= 0) throw new IOException("Non-positive argument");
+		if (n < 0) throw new IOException("Negative skip");
 		long res = in.skip(n);
-		if (res > 0) { pos += res; buf.clear(); }
+		if (res > 0) pos += res;
+		dirty = true;
 		return res < n ? -1 : res;
 	}
-	private int read_() throws IOException { int res = in.read(); if (res != -1) { pos++; buf.put(res); } return res; }
+	private int read_() throws IOException {
+		int res = in.read();
+		if (res != -1) { pos++; buf.put(res); }
+		return res;
+	}
 	private byte byte_(int i) { return buf.get(i); }
 	private int int_(int i) { return buf.getInt(i); }
-	private int readInt_(int _skip) throws IOException {
-		if (_skip < 0) throw new IOException("Negative argument");
-		else if (_skip > 0) if (skip_(_skip) == -1) return -1;
+//	private boolean check_(int i, char ... cs) { return buf.check(i, cs); }
+	private boolean check4_(int i, char c0, char c1, char c2, char c3) { return buf.check4(i, c0, c1, c2, c3); }
+	private boolean check4box_(char c0, char c1, char c2, char c3) { return (dirty = buf.check4(4, c0, c1, c2, c3)); }
+	private int read_(int nSkip, int nRead) throws IOException {
+		if (nSkip < 0) throw new IOException("Negative skip");
+		else if (nSkip > 0) if (skip_(nSkip) == -1) return -1;
 		buf.ptr = 0;
-		int res = in.read(buf.buf, 0, 4);
-		if (res > 0)
-			pos += res;
-		return res < 4 ? -1 : res;
+		int res = in.read(buf.buf, 0, nRead);
+		if (res > 0) pos += res;
+		dirty = true;
+		return res < nRead ? -1 : res;
 	}
-	private int int_() { int res = buf.getInt(0); buf.clear(); return res; }
+	private int readNext() throws IOException {
+		if (dirty) {
+			int res = read_(0, 8);
+			dirty = false;
+			return res;
+		} else
+			return read_();
+	}
 	
 	@Override
 	public int read(byte[] b, int off, int len) throws IOException {
@@ -49,44 +65,37 @@ public class fMP4DemuxerInputStream extends FilterInputStream implements Demuxer
 		public Box() { start = pos - 8; size = int_(0); }
 		public long skip() throws IOException { return skip_(start + size - pos); }
 	}
-	private Box moov;
-	private enum TrakType { unknown, video, audio }
-	private class Trak extends Box { TrakType type; int id, width, height, timeScale; }
-	private Trak trak;
-	private Map<Integer,Trak> traks = new HashMap<Integer,Trak>();
+	private enum TrakType { unknown, video, audio, other }
+	private class Trak extends Box {
+		TrakType type = TrakType.unknown;
+		int id, width, height, timeScale;
+		boolean done() { return type == TrakType.other || ((type == TrakType.video || type == TrakType.audio) && (traksByType.containsKey(type) || (id > 0 && timeScale > 0))); }
+		long finish(Box moov) throws IOException {
+			if ((type == TrakType.video || type == TrakType.audio) && !traksByType.containsKey(type)) {
+				traksById.put(id, this);
+				traksByType.put(type, this);
+				if (traksByType.size() == 2)
+					return moov.skip();
+			}			
+			return skip();
+		}
+	}
+	private Map<Integer,Trak> traksById = new HashMap<Integer,Trak>();
+	private Map<TrakType,Trak> traksByType = new HashMap<TrakType,Trak>();
 
 	@Override
 	public int read(MultiBuffer video, MultiBuffer audio) {
 		while (true) {
-			if (read_() == -1) return -1;
-			switch (byte_(4)) {
-			case (byte)'f': if (byte_(5) == (byte)'t' && byte_(6) == (byte)'y' && byte_(7) == (byte)'p') // ftyp
-				if (new Box().skip() == -1) return -1;
-			break;
-			case (byte)'m': if (byte_(5) == (byte)'o' && byte_(6) == (byte)'o') { if (byte_(7) == (byte)'v') // moov
-				moov = new Box();
-			else if (byte_(7) == (byte)'f') { // moof
+			if (readNext() == -1) return -1;
+			if (check4box_('m', 'o', 'o', 'f')) { // moof
 				
-			} } else if (byte_(5) == (byte)'d' && byte_(6) == (byte)'h' && byte_(7) == (byte)'d') { // mdhd
-				Box mdhd = new Box();
-				if (read_(20) == -1) return -1;
-				trak.timeScale = int_(0);
-				if (mdhd.skip() == -1) return -1;
-			} 
-			break;
-			case (byte)'t': if (byte_(5) == (byte)'r' && byte_(6) == (byte)'a' && byte_(7) == (byte)'k') // trak
-				trak = new Trak();
-			else if (byte_(5) == (byte)'k' && byte_(6) == (byte)'h' && byte_(7) == (byte)'d') { // tkhd
-				Box tkhd = new Box();
-				if (read_(20) == -1) return -1;
-				trak.id = int_(0);
-				if (read_(64) == -1) return -1;
-				trak.width = int_(0) >> 16;
-				trak.height = int_(4) >> 16;
-				if (tkhd.skip() == -1) return -1;
+			} else if (check4box_('m', 'o', 'o', 'v')) { // moov
+				if (read_moov(new Box()) == -1) return -1;
+			} else { // skip all other boxes
+				if (new Box().skip() == -1) return -1;
 			}
-			break;
-			}
+			
+
 			if (	(byte_(4) == (byte)'m' && ((byte_(5) == (byte)'d' && byte_(6) == (byte)'a' && byte_(7) == (byte)'t') || // mdat
 					((byte_(5) == (byte)'o' && byte_(6) == (byte)'o') && (byte_(7) == (byte)'f' || // moof
 					byte_(7) == (byte)'v')))) || // moov
@@ -102,8 +111,68 @@ public class fMP4DemuxerInputStream extends FilterInputStream implements Demuxer
 					pos += res;
 				return res == -1 || res < len_ - 8 ? -1 : len_;
 			}
+			
+			
 		}
 		return 0;
+	}
+
+	public int read_moov(Box moov) throws IOException {
+		while (true) {
+			if (readNext() == -1) return -1;
+			if (check4box_('t', 'r', 'a', 'k')) { // trak
+				if (read_trak(moov, new Trak()) == -1) return -1;
+			} else { // skip all other boxes
+				if (new Box().skip() == -1) return -1;
+			}
+			if (pos >= moov.start + moov.size)
+				return pos - (moov.start + moov.size);
+		}
+	}
+
+	public int read_trak(Box moov, Trak trak) throws IOException {
+		while (true) {
+			if (readNext() == -1) return -1;
+			if (check4box_('m', 'd', 'i', 'a')) { // mdia
+				if (read_mdia(moov, trak, new Box()) == -1) return -1;
+			} else if (check4box_('t', 'k', 'h', 'd')) { // tkhd
+				Box tkhd = new Box();
+				if (read_(12, 4) == -1) return -1;
+				trak.id = int_(0);
+				if (read_(60, 8) == -1) return -1;
+				trak.width = int_(0) >> 16;
+				trak.height = int_(4) >> 16;
+				if (trak.done()) { if (trak.finish(moov) == -1) return -1; }
+				else if (tkhd.skip() == -1) return -1;
+			} else { // skip all other boxes
+				if (new Box().skip() == -1) return -1;
+			}
+			if (pos >= trak.start + trak.size)
+				return pos - (trak.start + trak.size);
+		}
+	}
+
+	public int read_mdia(Box moov, Trak trak, Box mdia) throws IOException {
+		while (true) {
+			if (readNext() == -1) return -1;
+			if (check4box_('m', 'd', 'h', 'd')) { // mdhd
+				Box mdhd = new Box();
+				if (read_(12, 4) == -1) return -1;
+				trak.timeScale = int_(0);
+				if (trak.done()) { if (trak.finish(moov) == -1) return -1; }
+				else if (mdhd.skip() == -1) return -1;
+			} else if (check4box_('h', 'd', 'l', 'r')) { // hdlr
+				Box hdlr = new Box();
+				if (read_(8, 4) == -1) return -1;
+				trak.type = check4_(0, 'v', 'i', 'd', 'e') ? TrakType.video : check4_(0, 's', 'o', 'u', 'n') ? TrakType.audio : TrakType.other;
+				if (trak.done()) { if (trak.finish(moov) == -1) return -1; }
+				else if (hdlr.skip() == -1) return -1;
+			} else { // skip all other boxes
+				if (new Box().skip() == -1) return -1;
+			}
+			if (pos >= mdia.start + mdia.size)
+				return pos - (mdia.start + mdia.size);
+		}
 	}
 	
 //	public static void main(String[] args) throws IOException {
